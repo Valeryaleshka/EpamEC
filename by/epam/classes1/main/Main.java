d3d642d7f823450d676e2674abe710a87252d9db
Valeryaleshka <53650916+Valeryaleshka@users.noreply.github.com>
2019-10-14 15:03:48 +0300

    e
    rr

---
+++ by/epam/classes1/main/Main.java
@@ -0,0 +1,11 @@
+package by.epam.classes1.main;
+
+public class Main {
+    public static void main (String [] args){
+        Test1 number = new Test1(7,5);
+        Logic logic = new Logic();
+
+        System.out.println(logic.getSum(number));
+        System.out.println(logic.getBiggerNumber(number));
+    }
+}
---
+++ by/epam/classes1/main/Test1.java
@@ -0,0 +1,34 @@
+package by.epam.classes1.main;
+
+public class Test1 {
+    private int x;
+    private int y;
+
+    public Test1(){
+        this.x = 0;
+        this.y = 0;
+
+    }
+
+    public Test1(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX (int x){
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
