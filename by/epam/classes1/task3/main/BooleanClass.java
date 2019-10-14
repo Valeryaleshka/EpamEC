5b055d5b9118e4e2d634abbaa64d51b8394cf1f5
Valeryaleshka <53650916+Valeryaleshka@users.noreply.github.com>
2019-10-14 17:32:08 +0300

    +
    +

---
+++ by/epam/classes1/task3/main/BooleanClass.java
@@ -0,0 +1,12 @@
+package by.epam.classes1.task3.main;
+
+public class BooleanClass {
+    public boolean aBoolean (int [] numbers){
+        for (int i = 0; i < numbers.length;i++) {
+            if(numbers[i]!=9&&numbers[i]!=10){
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ by/epam/classes1/task3/main/View.java
@@ -0,0 +1,17 @@
+package by.epam.classes1.task3.main;
+
+public class View {
+    public void printStudentNameAndGroup(Student student){
+        System.out.print(student.getName()+"  "+student.getGroup()+"  ");
+
+    }
+    public void printStudentsWith9And10Marks(Student [] students){
+        BooleanClass booleanClass = new BooleanClass();
+        for (int i = 0; i<students.length;i++) {
+            if(booleanClass.aBoolean(students[i].getMarks())){
+                printStudentNameAndGroup(students[i]);
+                System.out.println();
+            }
+        }
+    }
+}
