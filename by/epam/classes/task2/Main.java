1bc868d38ae18fa46543e970c44f9c39eea03b64
Valeryaleshka <valeryaleshka@gmail.com>
2019-10-28 14:48:59 +0300

    +
    +

---
+++ by/epam/classes/task2/Main.java
@@ -0,0 +1,13 @@
+/*2. Создйте класс Test2 двумя переменными. Добавьте конструктор с входными параметрами. Добавьте
+конструктор, инициализирующий члены класса по умолчанию. Добавьте set- и get- методы для полей экземпляра
+класса.*/
+package by.epam.classes.task2;
+
+public class Main {
+    public static void main (String[] args){
+
+        Test2 test2 = new Test2();
+        Test2 test21 = new Test2(3,5);
+
+    }
+}
---
+++ by/epam/classes/task2/Test2.java
@@ -0,0 +1,35 @@
+package by.epam.classes.task2;
+
+public class Test2 {
+    private int x;
+    private int y;
+
+    public Test2(){
+        this.x = 0;
+        this.y = 0;
+
+    }
+
+    public Test2(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX (int x){
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
+
