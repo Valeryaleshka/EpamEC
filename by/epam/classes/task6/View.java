597f757c499bd2889466cc7a247b655e22cc8944
Valeryaleshka <valeryaleshka@gmail.com>
2019-10-28 15:03:55 +0300

    +
    +

---
+++ by/epam/classes/task6/View.java
@@ -0,0 +1,8 @@
+package by.epam.classes.task6;
+
+public class View {
+
+    public void printTime (Time time){
+        System.out.println(time.getHour()+"ч. "+time.getMin()+"м. "+time.getSec()+"с.");
+    }
+}
